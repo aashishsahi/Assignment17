8c1084a823583ddd80735ee3988c3b3c0faecd3e
Aashish <aashishsahi@gmail.com>
2018-06-28 12:46:26 +0530

    First Commit

---
+++ app/src/main/java/com/example/hp/assignment17/LoginPage.java
@@ -0,0 +1,17 @@
+package com.example.hp.assignment17;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        Toast.makeText(this, "Login Page", Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/example/hp/assignment17/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hp.assignment17;
+
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
